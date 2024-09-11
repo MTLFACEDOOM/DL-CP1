611351eb31d168668b412d929251f4abb4faf208
MTLFACEDOOM <devin.longtree@ucas-edu.net>
2024-09-11 10:15:57 -0600

    Finish

--- who_are_you/main.py
+++ who_are_you/main.py
@@ -1,7 +1,7 @@
-#Devin Longtree Who Are You
+#Devin Longtree, Who Are You?
 
 name = input("What is your name?: ")
-age = input(" What is your age?: ")
+age = input("What is your age?: ")
 color = input("What is your favorite color?: ")
 
-print
+print("Hello,",name, "You are", age, "years old, and your Favorite color is",color,)
