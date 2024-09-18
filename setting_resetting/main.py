694e08804c6e73339ae5bfecb1e4ba9e6e97e832
MTLFACEDOOM <devin.longtree@ucas-edu.net>
2024-09-18 10:23:53 -0600

    save

--- setting_resetting/main.py
+++ setting_resetting/main.py
@@ -1,12 +1,18 @@
+#Devin Longtree Setting/Resetting
+
 staff = input("How many faculty and staff are attending?: ")
 stud= input("How many students are attending?: ")
+guest = input("How many students are bringing guests?: ")
 
 staff = int(staff)
 stud = int(stud)
+guest = int(guest)
 
-total = (stud*3) + staff
+total = stud + staff
 
-table = total%12
+table = total/12
+
+table = round(table)
 
 print(total, "students, staff, and guests are attending."
       "You will need", table, "tables to hold all of your attendees."
