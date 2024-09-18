64b05196ce58aa1f788665d2169434dd75b0840b
MTLFACEDOOM <devin.longtree@ucas-edu.net>
2024-09-18 10:50:54 -0600

    madlib

---
+++ madlib/main.py
@@ -0,0 +1,16 @@
+noun1 = input("Give me a singular noun: ")
+noun2 = input("Give me a plural noun: ")
+noun3 = input("Give me a singular noun: ")
+verb1 = input("Give me a verb: ")
+verb2 = input("Give me a verb ending with 'ed': ")
+verb3 = input("Give me a verb: ")
+adj1 = input("Give me an adjective: ")
+adj2 = input("Give me an adjective: ")
+adj3 = input("Give me an adjective: ")
+
+print("The", adj1, "man wanted to", verb1, "to the store. When the", adj1, 
+    "man got to the store, the shopkeeper said 'You can't be here, your part of the", noun2,
+    "and i don't like", adj2, noun2, "in my store. the", adj1, "man, feeling dejected,", verb2, 
+    "away from the store. Later that night the", adj1, "man decided to try to get revenge. he decided to", verb3,
+    "the shopkeeper to get back at him. the man came back to the store anfd said that the shopkeeper was a", adj3, noun3
+      )
