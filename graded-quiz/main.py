8323b81625423c7f655b15118b7ffa07da5a7b1a
MTLFACEDOOM <devin.longtree@ucas-edu.net>
2024-09-25 10:38:21 -0600

    final

--- graded-quiz/main.py
+++ graded-quiz/main.py
@@ -1,7 +1,36 @@
+score = 0
 answer1 = 30
-question1 = input("What is 5x6?: ")
+answer2 = "bangkok"
+answer3 = 42
+answer4 = "neptunium"
+answer5 = "why not"
 
-if(question1 == answer1
-   return
+q1 = input("What is 5x6?: ")
+q1 = int(q1)
+if q1 == answer1:
+    score += 1
+else: print("You suck. Womp Womp")
 
-)
+q2 = input("What is the capital of Thailand?: ")
+if q2 == answer2:
+    score += 1
+else: print("You suck. Womp Womp")
+
+q3 = input("What is the meaning of life?: ")
+q3 = int(q3)
+if q3 == answer3:
+    score += 1
+else: print("You suck. Womp Womp")
+
+q4 = input("What element is Uranium-235 after it has undergone beta radiation?: ")
+if q4 == answer4:
+    score += 1
+else: print("You suck. Womp Womp")
+
+q5 = input("Why?: ")
+if q5 == answer5:
+    score += 1
+else: print("You suck. Womp Womp")
+
+
+print("You final score is... ", score)
--- simple_calculator/main.py
+++
@@ -1,9 +0,0 @@
-num1 = input("Input your first number: ")
-num2 = input("Input your second number: ")
-
-num1 = int(num1)
-num2 = int(num2)
-
-total = num1 + num2
-
-print(total)
