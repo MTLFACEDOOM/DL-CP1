80777417238cc06ac524650b4e25ad7de345df18
MTLFACEDOOM <devin.longtree@ucas-edu.net>
2024-09-26 10:50:33 -0600

    save

---
+++ personal_info_converter/mian.py
@@ -0,0 +1,6 @@
+name = input("What is your name>: ")
+age = input("What is your age?: ")
+height = input("What is your height (In meters)?: ")
+fav_num = input("What is your favorite number?: ")
+
+age = int(age)
