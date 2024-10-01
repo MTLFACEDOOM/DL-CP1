076d9bd4a8dc803a5b65bc9c836b04d58910ba21
MTLFACEDOOM <devin.longtree@ucas-edu.net>
2024-10-01 10:36:50 -0600

    final

--- average_grade/main.py
+++ average_grade/main.py
@@ -1,3 +1,5 @@
+#Average Grade Devin Longtree
+
 grade1 = input("What is your grade percentage is your first period class?: ")
 grade2 = input("What is your grade percentage is your second period class?: ")
 grade3 = input("What is your grade percentage is your third period class?: ")
@@ -6,16 +8,17 @@ grade6 = input("What is your grade percentage is your sixth period class?: ")
 grade7 = input("What is your grade percentage is your seventh period class?: ")
 grade8 = input("What is your grade percentage is your eighth period class?: ")
 
-grade1 = int(grade1)
-grade2 = int(grade2)
-grade3 = int(grade3)
-grade_4_5 = int(grade_4_5)
-grade6 = int(grade6)
-grade7 = int(grade7)
-grade8 = int(grade8)
+grade1 = float(grade1)
+grade2 = float(grade2)
+grade3 = float(grade3)
+grade_4_5 = float(grade_4_5)
+grade6 = float(grade6)
+grade7 = float(grade7)
+grade8 = float(grade8)
 
 total = grade1 + grade2 + grade3 + grade_4_5 + grade6 + grade7 + grade8
 
-average = (total//7)
+average = (total/7)
+round_av = round(average, 2)
 
-print("your average grade percent is:", average)
+print("your average grade percent is:", round_av)
---
+++ secret_cypher/main.py
@@ -0,0 +1,6 @@
+word = input("Input a word or phrase: ")
+
+def cypher(word):
+    word = word+" A "
+
+print(cypher)
