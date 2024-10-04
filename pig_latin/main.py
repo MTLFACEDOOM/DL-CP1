10e5a2eb9859916e326166e200e2543e212cd402
MTLFACEDOOM <devin.longtree@ucas-edu.net>
2024-10-04 10:48:11 -0600

    final

--- pig_latin/main.py
+++ pig_latin/main.py
@@ -1,4 +1,5 @@
-error = 0
+#Devin Longtree Pig Latin
+
 main = input("Input a word (all lowercase, no spaces): ")
 
 pig = main[0:1]
@@ -6,6 +7,13 @@ pig2 = list(main)
 pig2.pop(0)
 pig3 = "".join(pig2)
 
-if pig == "a", "e", "i", 
+def vowel():
+    print(main + "yay")
 
-print(pig3 + pig + "ay")
+def other():
+    print(pig3 + pig + "ay")
+
+if pig == "a" or pig == "e" or pig == "i" or pig == "o" or pig == "u":
+    vowel()
+else:
+    other()
--- test_things/main.py
+++ test_things/main.py
@@ -4,3 +4,4 @@ my_list = ['Hello', 'World']
 my_string = ' '.join(my_list)
 
 print(my_string)
+#if pig ==  "b" or " c" or "d" or "f" or "g" or "h" or "j" or "k" or "i" or "m" or "n" or "p" or "q" or "r" or "s" or "t" or "v" or "w" or "x" or "y" or "z":
