87b84888c8a74d3ce91dedb81337e7dbbc51bb79
MTLFACEDOOM <devin.longtree@ucas-edu.net>
2024-10-07 10:42:51 -0600

    begin

--- test_things/main.py
+++ test_things/main.py
@@ -1,7 +1,5 @@
-my_list = ['Hello', 'World']
+def add_to_list(item, my_list=[]):
 
-# Convert the list to a string
-my_string = ' '.join(my_list)
+     my_list.append(item)
 
-print(my_string)
-#if pig ==  "b" or " c" or "d" or "f" or "g" or "h" or "j" or "k" or "i" or "m" or "n" or "p" or "q" or "r" or "s" or "t" or "v" or "w" or "x" or "y" or "z":
+     return my_list 
