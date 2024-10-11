068937c0250ae5d42fb9cfbda983cca67cb7be49
MTLFACEDOOM <devin.longtree@ucas-edu.net>
2024-10-11 10:20:09 -0600

    final

--- what_numbers/main.py
+++ what_numbers/main.py
@@ -1,7 +1,7 @@
 #Devin Longtree What are these numbers?
 
 
-"""#Begin code for num1 (comma)
+#Begin code for num1 (comma)
 num1 = int(input("Input a number (four long): "))
 num1 = str(num1)
 length = len(num1)
@@ -16,13 +16,12 @@ all1 = "".join(all1)
 
 if length == 4:
     print(all1)
-    
-#end code for num1 (comma)"""
+#end code for num1 (comma)
 
-"""#Begin code for num2 (decimal)
+#Begin code for num2 (decimal)
 num2 = float(input("Input a decimal number with for numbers after the decimal: "))
 print(num2)
-#End code for num2 (decimal)"""
+#End code for num2 (decimal)
 
 #Begin code for num3 (Percent)
 num3 = int(input("Input a number (two long)"))
@@ -31,4 +30,19 @@ num3_1 = num3,"%"
 num3_1 = "".join(num3_1)
 
 print(num3_1)
-#Begin code for num3 (Percent)
+#End code for num3 (Percent)
+
+#Begin code for num4
+num4 = int(input("Input a number (three long): "))
+num4 = str(num4)
+
+num4_1 = num4[0:1]
+num4_2 = list(num4)
+num4_2.pop(0)
+num4_3 = "".join(num4_2)
+
+all4 = "$",num4_1,".",num4_3
+all4 = "".join(all4)
+
+print(all4)
+#End code for num4
