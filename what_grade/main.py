d7c5a0d999aed0407babb6ebc8c65e7496355999
MTLFACEDOOM <devin.longtree@ucas-edu.net>
2024-11-04 10:20:24 -0700

    final

--- what_grade/main.py
+++ what_grade/main.py
@@ -1,15 +1,17 @@
+#Devin longtree [What is my Grade?]
+
 percent = input("What is your cuurnt grade percentage (two long)?: ")
 percent = int(percent)
 
-if percent > 0 and percent < 59:
+if percent > 0 and percent < 60:
     print("You have an F")
-elif percent > 60 and percent < 69:
+elif percent > 59 and percent < 70:
     print("You have a D")
-elif percent > 70 and percent < 79:
+elif percent > 69 and percent < 80:
     print("You have a C")
-elif percent > 80 and percent < 89:
+elif percent > 79 and percent < 90:
     print("You have a B")
-elif percent > 90 and percent < 100:
+elif percent > 89 and percent < 101:
     print("You have an A")
 else:
     print("That is not possible to calculate.")
