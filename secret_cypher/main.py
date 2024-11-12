a58e83bbd926b4a503281722bf0a83e27a5bf321
MTLFACEDOOM <devin.longtree@ucas-edu.net>
2024-11-12 10:50:18 -0700

    save

--- secret_cypher/main.py
+++ secret_cypher/main.py
@@ -1,98 +1,89 @@
 #Devin Longtree [Secret Cypher]
 
-import random
-name = list(input("Input a word: "))
-name_len = len(name)
-def change_a(name):
-    if 'a' in name:
-        name[random.randint(1, name_len)] = "d"
-def change_b(name):
-    if 'b' in name:
-        name[random.randint(1, name_len)] = "e"
-def change_c(name):
-    if 'c' in name:
-        name[random.randint(1, name_len)] = "f"
-def change_d(name):
-    if 'd' in name:
-        name[random.randint(1, name_len)] = "g"
-def change_e(name):
-    if 'e' in name:
-        name[random.randint(1, name_len)] = "d"
-def change_f(name):
-    if 'f' in name:
-        name[random.randint(1, name_len)] = "e"
-def change_g(name):
-    if 'g' in name:
-        name[random.randint(1, name_len)] = "f"
-def change_h(name):
-    if 'h' in name:
-        name[random.randint(1, name_len)] = "g"
-def change_i(name):
-    if 'i' in name:
-        name[random.randint(1, name_len)] = "d"
-def change_j(name):
-    if 'j' in name:
-        name[random.randint(1, name_len)] = "e"
-def change_k(name):
-    if 'k' in name:
-        name[random.randint(1, name_len)] = "f"
-def change_l(name):
-    if 'l' in name:
-        name[random.randint(1, name_len)] = "g"
-def change_m(name):
-    if 'm' in name:
-        name[random.randint(1, name_len)] = "d"
-def change_n(name):
-    if 'n' in name:
-        name[random.randint(1, name_len)] = "e"
-def change_o(name):
-    if 'o' in name:
-        name[random.randint(1, name_len)] = "f"
-def change_p(name):
-    if 'p' in name:
-        name[random.randint(1, name_len)] = "g"
-def change_q(name):
-    if 'q' in name:
-        name[random.randint(1, name_len)] = "d"
-def change_r(name):
-    if 'r' in name:
-        name[random.randint(1, name_len)] = "e"
-def change_s(name):
-    if 'c' in name:
-        name[random.randint(1, name_len)] = "f"
-def change_t(name):
-    if 'd' in name:
-        name[random.randint(1, name_len)] = "g"
-def change_u(name):
-    if 'a' in name:
-        name[random.randint(1, name_len)] = "d"
-def change_v(name):
-    if 'b' in name:
-        name[random.randint(1, name_len)] = "e"
-def change_w(name):
-    if 'c' in name:
-        name[random.randint(1, name_len)] = "f"
-def change_x(name):
-    if 'd' in name:
-        name[random.randint(1, name_len)] = "g"
-def change_y(name):
-    if 'c' in name:
-        name[random.randint(1, name_len)] = "f"
-def change_z(name):
-    if 'd' in name:
-        name[random.randint(1, name_len)] = "g"
+word = input("Input a word: ")
+wordlist = list(word)
 
 
-
-
-
-
-
-
-print(name)
-print(name_len)
-name = "".join(name)
-print(name)
-
-
-
+def shift():
+    if "a" in word:
+        wordlist.remove("a")
+        wordlist.append("b")
+    if "b" in word:
+        wordlist.remove("b")
+        wordlist.append("c")
+    if "c" in word:
+        wordlist.remove("c")
+        wordlist.append("d")
+    if "d" in word:
+        wordlist.remove("d")
+        wordlist.append("e")
+    if "e" in word:
+        wordlist.remove("e")
+        wordlist.append("f")
+    if "f" in word:
+        wordlist.remove("f")
+        wordlist.append("g")
+    if "g" in word:
+        wordlist.remove("g")
+        wordlist.append("h")
+    if "h" in word:
+        wordlist.remove("h")
+        wordlist.append("i")
+    if "i" in word:
+        wordlist.remove("i")
+        wordlist.append("j")
+    if "j" in word:
+        wordlist.remove("j")
+        wordlist.append("k")
+    if "k" in word:
+        wordlist.remove("k")
+        wordlist.append("l")
+    if "l" in word:
+        wordlist.remove("l")
+        wordlist.append("m")
+    if "m" in word:
+        wordlist.remove("m")
+        wordlist.append("n")
+    if "n" in word:
+        wordlist.remove("n")
+        wordlist.append("o")
+    if "o" in word:
+        wordlist.remove("o")
+        wordlist.append("p")
+    if "p" in word:
+        wordlist.remove("p")
+        wordlist.append("q")
+    if "q" in word:
+        wordlist.remove("q")
+        wordlist.append("r")
+    if "r" in word:
+        wordlist.remove("r")
+        wordlist.append("s")
+    if "s" in word:
+        wordlist.remove("s")
+        wordlist.append("t")
+    if "t" in word:
+        wordlist.remove("t")
+        wordlist.append("u")
+    if "u" in word:
+        wordlist.remove("u")
+        wordlist.append("v")
+    if "v" in word:
+        wordlist.remove("v")
+        wordlist.append("w")
+    if "w" in word:
+        wordlist.remove("w")
+        wordlist.append("x")
+    if "x" in word:
+        wordlist.remove("x")
+        wordlist.append("y")
+    if "y" in word:
+        wordlist.remove("y")
+        wordlist.append("z")
+    if "z" in word:
+        wordlist.remove("z")
+        wordlist.append("a")
+print(word)
+shift()
+wordlist = ''.join(wordlist)
+print(wordlist)
--- test_things/main.py
+++ test_things/main.py
@@ -1,18 +1,123 @@
 import random
 name = list(input("Input a word: "))
 name_len = len(name)
-
-def change_x(name):
-    if 'h' in name:
-        name[random.randint(1, name_len)] = "g"
+def change_a(name):
+    if 'a' in name:
+        name[random.randint(0, name_len)] = "d"
+def change_b(name):
+    if 'b' in name:
+        name[random.randint(0, name_len)] = "e"
+def change_c(name):
+    if 'c' in name:
+        name[random.randint(0, name_len)] = "f"
+def change_d(name):
+    if 'd' in name:
+        name[random.randint(0, name_len)] = "g"
+def change_e(name):
     if 'e' in name:
-        name[random.randint(1, name_len)] = "f"
+        name[random.randint(0, name_len)] = "d"
+def change_f(name):
+    if 'f' in name:
+        name[random.randint(0, name_len)] = "e"
+def change_g(name):
+    if 'g' in name:
+        name[random.randint(0, name_len)] = "f"
+def change_h(name):
+    if 'h' in name:
+        name[random.randint(0, name_len)] = "g"
+def change_i(name):
+    if 'i' in name:
+        name[random.randint(0, name_len)] = "d"
+def change_j(name):
+    if 'j' in name:
+        name[random.randint(0, name_len)] = "e"
+def change_k(name):
+    if 'k' in name:
+        name[random.randint(0, name_len)] = "f"
+def change_l(name):
     if 'l' in name:
-        name[random.randint(1, name_len)] = "o"
-    if 'l' in name:
-        name[random.randint(1, name_len)] = "p"
+        name[random.randint(0, name_len)] = "g"
+def change_m(name):
+    if 'm' in name:
+        name[random.randint(0, name_len)] = "d"
+def change_n(name):
+    if 'n' in name:
+        name[random.randint(0, name_len)] = "e"
+def change_o(name):
+    if 'o' in name:
+        name[random.randint(0, name_len)] = "f"
+def change_p(name):
+    if 'p' in name:
+        name[random.randint(0, name_len)] = "g"
+def change_q(name):
+    if 'q' in name:
+        name[random.randint(0, name_len)] = "d"
+def change_r(name):
+    if 'r' in name:
+        name[random.randint(0, name_len)] = "e"
+def change_s(name):
+    if 'c' in name:
+        name[random.randint(0, name_len)] = "f"
+def change_t(name):
+    if 'd' in name:
+        name[random.randint(0, name_len)] = "g"
+def change_u(name):
+    if 'a' in name:
+        name[random.randint(0, name_len)] = "d"
+def change_v(name):
+    if 'b' in name:
+        name[random.randint(0, name_len)] = "e"
+def change_w(name):
+    if 'c' in name:
+        name[random.randint(0, name_len)] = "f"
+def change_x(name):
+    if 'd' in name:
+        name[random.randint(0, name_len)] = "g"
+def change_y(name):
+    if 'c' in name:
+        name[random.randint(0, name_len)] = "f"
+def change_z(name):
+    if 'd' in name:
+        name[random.randint(0, name_len)] = "g"
 
-change_x(name)
+def play():
+    change_a(name)
+    change_b(name)
+    change_c(name)
+    change_d(name)
+    change_e(name)
+    change_f(name)
+    change_g(name)
+    change_h(name)
+    change_i(name)
+    change_j(name)
+    change_k(name)
+    change_l(name)
+    change_m(name)
+    change_n(name)
+    change_o(name)
+    change_p(name)
+    change_q(name)
+    change_r(name)
+    change_s(name)
+    change_t(name)
+    change_u(name)
+    change_v(name)
+    change_w(name)
+    change_x(name)
+    change_y(name)
+    change_z(name)
+"""def change_b(name):
+    if 'e' in name:
+        name[random.randint(1, name_len+1)] = "r"
+def change_c(name):
+    if 'l' in name:
+        name[random.randint(1, name_len+1)] = "g"
+def change_d(name):
+    if 'o' in name:
+        name[random.randint(1, name_len+1)] = "p"""
+play()
 print(name)
+print (name_len)
 name = "".join(name)
 print(name)
