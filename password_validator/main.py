037eb333b02ed2992f8d532e246a83e04dade9b1
MTLFACEDOOM <devin.longtree@ucas-edu.net>
2024-11-18 10:50:55 -0700

    save

---
+++ password_validator/main.py
@@ -0,0 +1,14 @@
+#Devin Longtree [Password Validator]
+
+password = list(input("Enter a passsword: "))
+length = len(password)
+
+if length < 8:
+    print("Your password is not long enough")
+if "@" not in password:
+    if 
+if "1" not in password:
+    print("Your password is not strong enough, I recommend putting a number.")
+
+
+
