d9c566fd21b61dc684a229adc19bc4eac2a7e137
MTLFACEDOOM <devin.longtree@ucas-edu.net>
2024-11-19 10:51:03 -0700

    save

---
+++ simple_quiz_game/main.py
@@ -0,0 +1,20 @@
+#Devin Longtree [Simple Quiz Game]
+
+print("Wecome to a quiz of sorts")
+
+question1 = input("""First Question: question 1
+      a. answer
+      b. answer
+      c. answer
+      d. correct answer
+    
+      """)
+
+if question1 == "a":
+    print("Incorrect")
+    if question1 == "b":
+        print("Incorrect") 
+    if question1 == "c":
+        print("Incorrect")
+        if question1 == "a":
+            print("Incorrect")
