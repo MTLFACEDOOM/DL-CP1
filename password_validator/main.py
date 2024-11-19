6a90a319a4fa75fbbad4f0df51e13f0368cf7606
MTLFACEDOOM <devin.longtree@ucas-edu.net>
2024-11-19 10:42:31 -0700

    save

--- password_validator/main.py
+++ password_validator/main.py
@@ -1,14 +1,15 @@
 #Devin Longtree [Password Validator]
 
+
 password = list(input("Enter a passsword: "))
 length = len(password)
 
-if length < 8:
-    print("Your password is not long enough")
-if "@" not in password:
-    if 
-if "1" not in password:
-    print("Your password is not strong enough, I recommend putting a number.")
+def check_length():
+    if length < 8:
+        print("Your password is not long enough")
+
+
+
 
 
 
---
+++ rock_paper_scissors/main.py
@@ -0,0 +1,15 @@
+#Devin Longtree [Rock Paper Scissors]
+
+
+import random
+cominput = random.randint(1,3)
+print(cominput)
+userinput = int(input("""Choose one:
+1. Rock
+2. Paper
+ 3. Scissors
+"""))
+
+
+
+
