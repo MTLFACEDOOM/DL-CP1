90c1c0fec640e389551ece319fd1922b4bdd47f6
MTLFACEDOOM <devin.longtree@ucas-edu.net>
2024-11-22 10:50:58 -0700

    save

--- simple_quiz_game/main.py
+++ simple_quiz_game/main.py
@@ -1,3 +1,28 @@
 #Devin Longtree [Simple Quiz Game]
+import random
+score = 0
+incorrect_answer = ["Oh, so close!", "Not quite right!", "Atleast you did your best.", "Dawg how did you even get this wrong."]
+correct_answer = ["Good Job!", "Correct!", "Not too hard now, is it?"]
+wahoo = random.choice(correct_answer)
+uhoh = random.choice(incorrect_answer)
 
-
+def questions():
+    global score
+    def q1():
+        question1 = input("""Question 1:
+    a. answer
+    b. answer
+    c. answer
+    d . correct answer
+        """)
+        if question1 == "d":
+                print(wahoo)
+                score = score + 1
+                print("Your score is...", score)
+                print("Will you get this next one right?")
+        else:
+            print(uhoh)
+            print("Lets give you an easier one!")
+    q1()
+    print("Did this work?")
+questions()
