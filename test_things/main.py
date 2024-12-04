1f856b1fcc32acdc5a3c1edd60f40ca7ca338e3a
MTLFACEDOOM <devin.longtree@ucas-edu.net>
2024-12-04 10:51:06 -0700

    save

--- test_things/main.py
+++ test_things/main.py
@@ -1,32 +1,4 @@
-import random
-cominput = random.randint(1,9)
-board1 = [1,2,3]
-board2 = [4,5,6]
-board3 = [7,8,9]
-print(board1)
-print(board2)
-print(board3)
-print(cominput)
-if cominput == 1:
-    board1[0] = "x"
-if cominput == 2:
-    board1[1] = "x"
-if cominput == 3:
-    board1[2] = "x"
+board1 = [1,"x",3]
 
-if cominput == 4:
-    board2[0] = "x"
-if cominput == 5:
-    board2[1] = "x"
-if cominput == 6:
-    board2[2] = "x"
-
-if cominput == 7:
-    board3[0] = "x"
-if cominput == 8:
-    board3[1] = "x"
-if cominput == 9:
-    board3[2] = "x"
+board1[0] = "x"
 print(board1)
-print(board2)
-print(board3)
--- tic_tac_toe/main.py
+++ tic_tac_toe/main.py
@@ -1,6 +1,105 @@
 #Devin longtree [Tic Tac Toe Game]
 import random
-cominput = random.randint(1,9)
 board1 = [1,2,3]
 board2 = [4,5,6]
 board3 = [7,8,9]
+print(board1)
+cominput = random.randint(1,3)
+print(cominput)
+
+def complay():
+    if cominput == 1:
+        if board1[0] == "x":
+            print("Sorry! This space is already taken!")
+        elif board1[0] != "x":
+            board1[0] = "x"
+            print(board1)
+
+    if cominput == 2:
+        if board1[1] == "x":
+            print("Sorry! This space is already taken!")    
+        elif board1[1] != "x":
+            board1[1] = "x"
+            print(board1)
+
+    if cominput == 3:
+        if board1[2] == "x":
+            print("Sorry! This space is already taken!")
+        elif board1[2] != "x":
+            board1[2] = "x"
+            print(board1)
+
+def userplay():
+    print(board1)
+    userinput = int(input("Please enter a number from 1-9: "))
+#board1
+    if userinput == 1:
+        if board1[0] == "x":
+            print("Sorry! This space is already taken!")
+        elif board1[0] != "x":
+            board1[0] = "x"
+            print(board1)
+
+    if userinput == 2:
+        if board1[1] == "x":
+            print("Sorry! This space is already taken!")    
+        elif board1[1] != "x":
+            board1[1] = "x"
+            print(board1)
+
+    if userinput == 3:
+        if board1[2] == "x":
+            print("Sorry! This space is already taken!")
+        elif board1[2] != "x":
+            board1[2] = "x"
+            print(board1)
+#board2
+    if userinput == 4:
+        if board1[0] == "x":
+            print("Sorry! This space is already taken!")
+        elif board1[0] != "x":
+            board1[0] = "x"
+            print(board1)
+
+    if userinput == 5:
+        if board1[1] == "x":
+            print("Sorry! This space is already taken!")    
+        elif board1[1] != "x":
+            board1[1] = "x"
+            print(board1)
+
+    if userinput == 3:
+        if board1[2] == "x":
+            print("Sorry! This space is already taken!")
+        elif board1[2] != "x":
+            board1[2] = "x"
+            print(board1)
+#board3
+    if userinput == 1:
+        if board1[0] == "x":
+            print("Sorry! This space is already taken!")
+        elif board1[0] != "x":
+            board1[0] = "x"
+            print(board1)
+
+    if userinput == 2:
+        if board1[1] == "x":
+            print("Sorry! This space is already taken!")    
+        elif board1[1] != "x":
+            board1[1] = "x"
+            print(board1)
+
+    if userinput == 3:
+        if board1[2] == "x":
+            print("Sorry! This space is already taken!")
+        elif board1[2] != "x":
+            board1[2] = "x"
+            print(board1)
+
+    choice = input("Do you want to play again? yes/no: ")
+    if choice == "yes":
+        userplay()
+    else:
+        print("Ok")
+
+userplay()
