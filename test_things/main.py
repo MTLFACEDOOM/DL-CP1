faf2c817457f6df3768fb2580812d89a1392a552
MTLFACEDOOM <devin.longtree@ucas-edu.net>
2024-12-05 21:43:39 -0700

    final

--- test_things/main.py
+++ test_things/main.py
@@ -2,91 +2,37 @@ import random
 board1 = [1,2,3]
 board2 = [4,5,6]
 board3 = [7,8,9]
-def complay():
-    cominput = random.randint(1,9)
-    print(cominput)
-    if cominput == 1:
+def userplay():
+    print(board1)
+    print(board2)
+    print(board3)
+    userinput = int(input("Please enter a number from 1-9: "))
+#board1
+    if userinput == 1:
         if board1[0] == "x" or board1[0] == "o":
-            print("Com Input error. Retrying.")
-            complay()
-        elif board1[0] != "x" or board1[0] != "o":
-            board1[0] = "o"
+            print("Sorry! This space is already taken!")
+        elif board1[0] != "x":
+            board1[0] = "x"
             print(board1)
             print(board2)
             print(board3)
-    if cominput == 2:
+
+    if userinput == 2:
         if board1[1] == "x" or board1[1] == "o":
-            print("Com Input error. Retrying.")
-            complay()
-        elif board1[1] != "x" or board1[1] != "o":
-            board1[1] = "o"
+            print("Sorry! This space is already taken!")    
+        elif board1[1] != "x":
+            board1[1] = "x"
             print(board1)
             print(board2)
             print(board3)
-    if cominput == 3:
+
+    if userinput == 3:
         if board1[2] == "x" or board1[2] == "o":
-            print("Com Input error. Retrying.")
-            complay()
-        elif board1[2] != "x" or board1[2] != "o":
-            board1[2] = "o"
+            print("Sorry! This space is already taken!")
+        elif board1[2] != "x":
+            board1[2] = "x"
             print(board1)
             print(board2)
             print(board3)
 
-    if cominput == 4:
-        if board2[0] == "x" or board2[0] == "o":
-            print("Com Input error. Retrying.")
-            complay()
-        elif board2[0] != "x" or board2[0] != "o":
-            board2[0] = "o"
-            print(board1)
-            print(board2)
-            print(board3)
-    if cominput == 5:
-        if board2[1] == "x" or board2[1] == "o":
-            print("Com Input error. Retrying.")
-            complay()
-        elif board2[1] != "x" or board2[1] != "o":
-            board2[1] = "o"
-            print(board1)
-            print(board2)
-            print(board3)
-    if cominput == 6:
-        if board2[2] == "x" or board2[2] == "o":
-            print("Com Input error. Retrying.")
-            complay()
-        elif board2[2] != "x" or board2[2] != "o":
-            board2[2] = "o"
-            print(board1)
-            print(board2)
-            print(board3)
-
-    if cominput == 7:
-        if board3[0] == "x" or board3[0] == "o":
-            print("Com Input error. Retrying.")
-            complay()
-        elif board3[0] != "x" or board3[0] != "o":
-            board3[0] = "o"
-            print(board1)
-            print(board2)
-            print(board3)
-    if cominput == 8:
-        if board3[1] == "x" or board3[1] == "o":
-            print("Com Input error. Retrying.")
-            complay()
-        elif board3[1] != "x" or board3[1] != "o":
-            board3[1] = "o"
-            print(board1)
-            print(board2)
-            print(board3)
-    if cominput == 9:
-        if board3[2] == "x" or board3[2] == "o":
-            print("Com Input error. Retrying.")
-            complay()
-        elif board3[2] != "x" or board3[2] != "o":
-            board3[2] = "o"
-            print(board1)
-            print(board2)
-            print(board3)
-
-complay()
+userplay()
--- tic_tac_toe/main.py
+++ tic_tac_toe/main.py
@@ -4,145 +4,168 @@ board1 = [1,2,3]
 board2 = [4,5,6]
 board3 = [7,8,9]
 cominput = random.randint(1,9)
+wincondition = False
 
 
 def userwin():
+    global wincondition
 #horizontal
     if board1[0] == "x" and board1[1] == "x" and board1[2] == "x":
         print("You Win!")
+        wincondition = True
     if board2[0] == "x" and board2[1] == "x" and board2[2] == "x":
         print("You Win!")
+        wincondition = True
     if board3[0] == "x" and board3[1] == "x" and board3[2] == "x":
         print("You Win!")
+        wincondition = True
 #Vertical
     if board1[0] == "x" and board2[0] == "x" and board3[0] == "x":
         print("You Win!")
+        wincondition = True
     if board1[1] == "x" and board2[1] == "x" and board3[1] == "x":
         print("You Win!")
+        wincondition = True
     if board1[2] == "x" and board2[2] == "x" and board3[2] == "x":
         print("You Win!")
+        wincondition = True
 #Diagonal
     if board1[0] == "x" and board2[1] == "x" and board3[2] == "x":
         print("You Win!")
+        wincondition = True
     if board1[2] == "x" and board2[1] == "x" and board3[0] == "x":
         print("You Win!")
+        wincondition = True
+        
+    
 
 def comwin():
-    #horizontal
+    global wincondition
+#horizontal
     if board1[0] == "o" and board1[1] == "o" and board1[2] == "o":
         print("Computer Wins!")
+        wincondition = True
     if board2[0] == "o" and board2[1] == "o" and board2[2] == "o":
         print("Computer Wins!")
+        wincondition = True
     if board3[0] == "x" and board3[1] == "o" and board3[2] == "o":
         print("Computer Wins!")
+        wincondition = True
 #Vertical
     if board1[0] == "o" and board2[0] == "o" and board3[0] == "o":
         print("Computer Wins!")
+        wincondition = True
     if board1[1] == "o" and board2[1] == "o" and board3[1] == "o":
         print("Computer Wins!")
+        wincondition = True
     if board1[2] == "o" and board2[2] == "o" and board3[2] == "o":
         print("Computer Wins!")
+        wincondition = True
 #Diagonal
     if board1[0] == "o" and board2[1] == "o" and board3[2] == "o":
-        print("You Win!")
+        print("YComputer Wins!")
+        wincondition = True
     if board1[2] == "o" and board2[1] == "o" and board3[0] == "o":
         print("Computer Wins!")
+        wincondition = True
+
+        
 #a
 def complay():
     cominput = random.randint(1,9)
     print(cominput)
     if cominput == 1:
-        if board1[0] == "x":
+        if board1[0] == "x" or board1[0] == "o":
             print("Com Input error. Retrying.")
             complay()
-        elif board1[0] != "x":
-            board1[0] = "x"
+        elif board1[0] != "x" or board1[0] != "o":
+            board1[0] = "o"
             print(board1)
             print(board2)
             print(board3)
-            win()
+            comwin()
     if cominput == 2:
-        if board1[1] == "x":
+        if board1[1] == "x" or board1[1] == "o":
             print("Com Input error. Retrying.")
             complay()
-        elif board1[1] != "x":
-            board1[1] = "x"
+        elif board1[1] != "x" or board1[1] != "o":
+            board1[1] = "o"
             print(board1)
             print(board2)
             print(board3)
-            win()
+            comwin()
     if cominput == 3:
-        if board1[2] == "x":
+        if board1[2] == "x" or board1[2] == "o":
             print("Com Input error. Retrying.")
             complay()
-        elif board1[2] != "x":
-            board1[2] = "x"
+        elif board1[2] != "x" or board1[2] != "o":
+            board1[2] = "o"
             print(board1)
             print(board2)
             print(board3)
-            win()
+            comwin()
 
     if cominput == 4:
-        if board2[0] == "x":
+        if board2[0] == "x" or board2[0] == "o":
             print("Com Input error. Retrying.")
             complay()
-        elif board2[0] != "x":
-            board2[0] = "x"
+        elif board2[0] != "x" or board2[0] != "o":
+            board2[0] = "o"
             print(board1)
             print(board2)
             print(board3)
-            win()
+            comwin()
     if cominput == 5:
-        if board2[1] == "x":
+        if board2[1] == "x" or board2[1] == "o":
             print("Com Input error. Retrying.")
             complay()
-        elif board2[1] != "x":
-            board2[1] = "x"
+        elif board2[1] != "x" or board2[1] != "o":
+            board2[1] = "o"
             print(board1)
             print(board2)
             print(board3)
-            win()
+            comwin()
     if cominput == 6:
-        if board2[2] == "x":
+        if board2[2] == "x" or board2[2] == "o":
             print("Com Input error. Retrying.")
             complay()
-        elif board2[2] != "x":
-            board2[2] = "x"
+        elif board2[2] != "x" or board2[2] != "o":
+            board2[2] = "o"
             print(board1)
             print(board2)
             print(board3)
-            win()
+            comwin()
 
     if cominput == 7:
-        if board3[0] == "x":
+        if board3[0] == "x" or board3[0] == "o":
             print("Com Input error. Retrying.")
             complay()
-        elif board3[0] != "x":
-            board3[0] = "x"
+        elif board3[0] != "x" or board3[0] != "o":
+            board3[0] = "o"
             print(board1)
             print(board2)
             print(board3)
-            win()
+            comwin()
     if cominput == 8:
-        if board3[1] == "x":
+        if board3[1] == "x" or board3[1] == "o":
             print("Com Input error. Retrying.")
             complay()
-        elif board3[1] != "x":
-            board3[1] = "x"
+        elif board3[1] != "x" or board3[1] != "o":
+            board3[1] = "o"
             print(board1)
             print(board2)
             print(board3)
-            win()
+            comwin()
     if cominput == 9:
-        if board3[2] == "x":
+        if board3[2] == "x" or board3[2] == "o":
             print("Com Input error. Retrying.")
             complay()
-        elif board3[2] != "x":
-            board3[2] = "x"
+        elif board3[2] != "x" or board3[2] != "o":
+            board3[2] = "o"
             print(board1)
             print(board2)
             print(board3)
-            win()
+            comwin()
 
 def userplay():
     print(board1)
@@ -151,99 +174,100 @@ def userplay():
     userinput = int(input("Please enter a number from 1-9: "))
 #board1
     if userinput == 1:
-        if board1[0] == "x":
+        if board1[0] == "x" or board1[0] == "o":
             print("Sorry! This space is already taken!")
         elif board1[0] != "x":
             board1[0] = "x"
             print(board1)
             print(board2)
             print(board3)
-            win()
+            userwin()
 
     if userinput == 2:
-        if board1[1] == "x":
+        if board1[1] == "x" or board1[1] == "o":
             print("Sorry! This space is already taken!")    
         elif board1[1] != "x":
             board1[1] = "x"
             print(board1)
             print(board2)
             print(board3)
-            win()
+            userwin()
 
     if userinput == 3:
-        if board1[2] == "x":
+        if board1[2] == "x" or board1[2] == "o":
             print("Sorry! This space is already taken!")
         elif board1[2] != "x":
             board1[2] = "x"
             print(board1)
             print(board2)
             print(board3)
-            win()
+            userwin()
 #board2
     if userinput == 4:
-        if board2[0] == "x":
+        if board2[0] == "x" or board2[0] == "o":
             print("Sorry! This space is already taken!")
         elif board2[0] != "x":
             board2[0] = "x"
             print(board1)
             print(board2)
             print(board3)
-            win()
+            userwin()
     if userinput == 5:
-        if board2[1] == "x":
+        if board2[1] == "x" or board2[1] == "o":
             print("Sorry! This space is already taken!")
         elif board2[1] != "x":
             board2[1] = "x"
             print(board1)
             print(board2)
             print(board3)
-            win()
+            userwin()
     if userinput == 6:
-        if board2[2] == "x":
+        if board2[2] == "x" or board2[2] == "o":
             print("Sorry! This space is already taken!")
         elif board2[2] != "x":
             board2[2] = "x"
             print(board1)
             print(board2)
             print(board3)
-            win()
+            userwin()
 #board3
     if userinput == 7:
-        if board3[0] == "x":
+        if board3[0] == "x" or board3[0] == "o":
             print("Sorry! This space is already taken!")
         elif board3[0] != "x":
             board3[0] = "x"
             print(board1)
             print(board2)
             print(board3)
-            win()
+            userwin()
     if userinput == 8:
-        if board3[1] == "x":
+        if board3[1] == "x" or board3[1] == "o":
             print("Sorry! This space is already taken!")    
         elif board3[1] != "x":
             board3[1] = "x"
             print(board1)
             print(board2)
             print(board3)
-            win()
+            userwin()
     if userinput == 9:
-        if board3[2] == "x":
+        if board3[2] == "x" or board3[2] == "o":
             print("Sorry! This space is already taken!")
         elif board3[2] != "x":
             board3[2] = "x"
             print(board1)
             print(board2)
             print(board3)
-            win()
+            userwin()
 
 def playloop():
+    global wincondition
     print("Computer Turn")
     complay()
     print("User Turn")
     userplay()
-    choice = input("Do you want to play again? yes/no: ")
-    if choice == "yes":
-        playloop()
-    else:
-        print("Ok")
+    if wincondition == True:
+        print("Thanks for Playing.")
+    if wincondition == False:
+            playloop()
+
 playloop()
